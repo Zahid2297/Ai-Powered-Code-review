8726bb6943b984b047ab8ab5a3691f1cef50b5a6
Mohammed Zahed <techpirate97@gmail.com>
2026-08-26 02:29:20 +0530

    add reposync and vector db which is pinecone and funtioins

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import { cn } from "@/lib/utils";
 import { ThemeProvider } from "@/components/providers/theme-provider";
 import { QueryProvider } from "@/components/providers/query-provider";
+import { Toaster } from "@/components/ui/toast";
 
 const outfitHeading = Outfit({
   subsets: ["latin"],
@@ -51,6 +52,7 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
             disableTransitionOnChange
           >
             {children}
+            <Toaster />
           </ThemeProvider>
         </QueryProvider>
       </body>
